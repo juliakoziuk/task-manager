faa6edb44c25a5e2ab1a0011dda4245e66c0da4e
juliakoziuk <test@gmail.com>
2024-11-19 13:24:35 +0200

    add express with body-parser

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = 3000;
+
+//middleware
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+  console.log(
+    `Server listening on port: ${port} and starting at http://localhost:${port}`,
+  );
+});
